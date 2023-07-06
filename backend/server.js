be298c39f37ef2f16fbb24bb0fb2763bd606ac0f
nihad512 <nihad0babayev@gmail.com>
2023-07-06 13:04:30 +0200

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('server');
+const express=require('express');
+const dotenv=require('dotenv').config();
+
+const PORT=process.env.PORT||5000
+
+const app =express();
+
+
+app.get('/',(req,res)=>{
+    res.json({message:'welcome to the Support API'})
+})
+//routes
+app.use('/api/users',require('./routes/userRoutes'))
+
+
+app.listen(PORT, ()=> console.log(`server started on port ${PORT}`));
